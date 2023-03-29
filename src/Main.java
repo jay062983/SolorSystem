9993fede5070b470ea2461b12d81e08a983c0b5c
jay062983 <jaycs0617@gmail.com>
2023-03-29 07:21:21 -0400

    The basic solarsystem is ready

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solarSystem=new SolarSystem();
+        solarSystem.sun="1";
+        solarSystem.moon="2";
+        solarSystem.planet="2";
+
+
+    }
+}
